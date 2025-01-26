d19674f13085bf47c55d4ac1b980eccb6c82d941
go-van-go <lukejbodmer@gmail.com>
2025-01-26 14:28:23 -0500

    fixed many things

--- flake.nix
+++ flake.nix
@@ -1,9 +1,28 @@
 {
-  description = "Test flake";
+  description = "A Nix-flake-based Python development environment";
 
-  outputs = { self }: {
-    devShell = { pkgs, ... }: pkgs.mkShell {
-      buildInputs = [ pkgs.hello ];
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ python311 ] ++
+            (with pkgs.python311Packages; [
+              pip
+              venvShellHook
+            ]);
+	  shellHook = ''
+            export VIRTUAL_ENV="Go Van Go"
+	  '';
+        };
+      });
     };
-  };
 }
